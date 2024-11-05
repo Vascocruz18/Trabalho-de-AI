3b091937691586886167f60ded57590336988a6e
Vascocruz18 <a14649@esars.pt>
2024-11-05 09:03:23 +0000

    Adicionei funções
    funções de soma, subtração, multiplicação e divisão.

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+# Função para soma
+def soma(x, y):
+    return x + y
+
+# Função para subtracao
+def subtracao(x, y):
+    return x - y
+
+# Função para multiplicacao
+def multiplicacao(x , y):
+    return x * y
+
+# Função para divisao
+def divisao(x, y):
+    return x / y
